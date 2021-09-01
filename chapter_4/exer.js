6cba0054b5a78e0af2c2072060135b66f4d80ffc
berlinen <1292393108@qq.com>
2021-09-02 01:10:37 +0800

    feat: 判别二叉搜索树是否一致

---
+++ chapter_4/exer.js
@@ -0,0 +1,23 @@
+// 判断两颗二叉搜索树是否一致
+
+class Node {
+  constructor(value) {
+    this.lChild = null
+    this.rChild = null
+    this.value = value
+    this.flag = 0  // 作为有没有被访问过的一个标记
+  }
+}
+
+function main() {
+  // 读入N和L
+  //  根据第一行序列建树T
+  // 依据树T分别判断后面的L个序列是否能与T形成同一个搜索树并输出结果
+
+  return  0
+}
+
+// 读数据建立搜索树T
+
+// 判别一序列是否与T构成一样的搜索树
+
