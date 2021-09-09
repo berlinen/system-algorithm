c2c001f2f624c36a32f370d2f9b4b392986b33df
berlinen <1292393108@qq.com>
2021-09-09 23:29:56 +0800

    feat: 最长公共前缀

--- chapter_5/exer.js
+++ chapter_5/exer.js
@@ -47,3 +47,20 @@ Trie.prototype.searchLongestPrefix = function() {
     }
     return prevs
 }
+
+// 解法一：逐个比较
+// 解题思路： 从前往后依次比较字符串，获取公共前缀
+
+var longestCommonPrefix = function(strs) {
+  if (strs === null || strs.length === 0) return "";
+  let prevs = strs[0]
+  for(let i = 1; i < strs.length; i++) {
+      let j = 0
+      for(; j < prevs.length && j < strs[i].length; j++) {
+          if(prevs.charAt(j) !== strs[i].charAt(j)) break
+      }
+      prevs = prevs.substring(0, j)
+      if(prevs === "") return ""
+  }
+  return prevs
+};
